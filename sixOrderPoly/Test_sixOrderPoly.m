fff5600c63d1b95487926018729e87d8900556a8
mx0922 <48557309+mx0922@users.noreply.github.com>
2021-11-04 14:35:43 +0800

    Update Test_sixOrderPoly.m

--- sixOrderPoly/Test_sixOrderPoly.m
+++ sixOrderPoly/Test_sixOrderPoly.m
@@ -1,19 +1,19 @@
 clear; close all; clc
 
-% 总时间
+% total time
 T = 1.0;
 
-% 开始时刻(t=0): 位置、速度、加速度
+% start time(t=0): pos, vel, acc
 p0 = 0;     v0 = 0;     a0 = 0;
-% 中间时刻(t=T/2): 位置
+% middle time(t=T/2):pos
 pm = 0.2;
-% 结束时刻(t=T): 位置、速度、加速度
+% end time(t=T):pos, vel, acc
 p1 = 0.5;   v1 = 0;     a1 = 0;
 
-% 六次多项式的系数
+% polynomials coefficients
 S = getPolyCoeff(T, p0, v0, a0, pm, p1, v1, a1);
 
-% 细分时刻各点的位置、速度、加速度
+% finer time node
 dt = 0.05;
 tSpan = 0:dt:T;
 
@@ -24,11 +24,11 @@ acc = zeros(Nt, 1);
 
 for i = 1:Nt
     t = tSpan(i);
-    % 得到对应时刻的位置、速度、加速度
+    % finer time node pos, vel, acc
     [pos(i), vel(i), acc(i)] = getSixOrderPoly(S, t);
 end
 
-% % 利用matlab的矩阵运算能力直接得到整段轨迹
+% % good vectorization
 % [pos, vel, acc] = getSixOrderPoly(S, tSpan');
 
 figure(1001); clf;
